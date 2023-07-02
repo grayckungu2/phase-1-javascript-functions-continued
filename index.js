9181ab821fae09218b30e68d836df70b4ffc7e5e
Grace kungu <grayckungu2@gmail.com>
2023-07-02 20:32:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${roller-skate}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${"go to the office"}.`
+}
+
+function wrapAdjective(string=" a dedicated programmer") {
+    return function(parameter="special") {
+        return `You are ${ "You are ||a dedicated programmer||!" }${ "a hard worker"}
+${ "You are *a hard worker*!" }!`
+    }
+}
